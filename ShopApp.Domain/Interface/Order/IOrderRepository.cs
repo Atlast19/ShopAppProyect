d4d300e01ad0d85b658789eee0807aa0054507b1
Atlast19 <ahleandro18@gmail.com>
2025-09-05 23:36:12 -0400

    EmployeesRepository completado

--- ShopApp.Domain/Interface/Order/IOrderRepository.cs
+++ ShopApp.Domain/Interface/Order/IOrderRepository.cs
@@ -1,11 +1,14 @@
-﻿
-using ShopApp.Domain.Entity;
-using ShopApp.Domain.Interface.Base;
+﻿using ShopApp.Domain.Base;
 using ShopApp.Domain.Models.Order.OrderBaseModel;
 
 namespace ShopApp.Domain.Interface
 {
-    public interface IOrderRepository : IBaseRepository<Order,OrderModel>
+    public interface IOrderRepository 
     {
+        Task<OperationResult<OrderModel>> CreateCategoriaAsync(OrderModel model);
+        Task<OperationResult<List<OrderModel>>> GetAllCategoriaAsync();
+        Task<OperationResult<OrderModel>> GetCategoriaByIdAsync(int id);
+        Task<OperationResult<OrderModel>> DeleteCategoriaByIdAsync(int id, int delete_user);
+        Task<OperationResult<OrderModel>> UpdateCategoria(OrderModel model);
     }
 }
--- ShopApp.Percistence/Repositories/Order/OrderRepository.cs
+++ ShopApp.Percistence/Repositories/Order/OrderRepository.cs
@@ -5,55 +5,32 @@ using ShopApp.Domain.Base;
 using ShopApp.Domain.Entity;
 using ShopApp.Domain.Interface;
 using ShopApp.Domain.Models.Order.OrderBaseModel;
-using System.Linq.Expressions;
 
 namespace ShopApp.Percistence.Repositories.Order
 {
     public class OrderRepository : IOrderRepository
     {
-        private readonly ShopDbAppContext _context;
-        private readonly ILogger<OrderRepository> _logger;
-
-        public OrderRepository(ShopDbAppContext context, ILogger<OrderRepository> logger)
-        {
-            _context = context;
-            _logger = logger;
-        }
-        public async Task AddAsync(Domain.Entity.Order entity)
+        public Task<OperationResult<OrderModel>> CreateCategoriaAsync(OrderModel model)
         {
             throw new NotImplementedException();
         }
 
-        public async Task DeleteAsync(int id)
+        public Task<OperationResult<OrderModel>> DeleteCategoriaByIdAsync(int id, int delete_user)
         {
             throw new NotImplementedException();
         }
 
-        public async Task<bool> ExistsAsync(Expression<Func<Domain.Entity.Order, bool>> filter)
+        public Task<OperationResult<List<OrderModel>>> GetAllCategoriaAsync()
         {
             throw new NotImplementedException();
         }
 
-        public async Task<OperationResult<OrderModel>> GetAllAsync()
-        {
-            _logger.LogInformation("Cargando todas las ordenes");
-            var order = await _context.Orders.ToListAsync();
-            if (order is null)
-            {
-                return OperationResult<OrderModel>.Failure("No se encuentran datos en la tabla");
-            }
-            else
-            {
-            return OperationResult<OrderModel>.Succes("Datos cargados",order);
-            }
-        }
-
-        public async Task<OrderModel?> GetByIdAsync(int id)
+        public Task<OperationResult<OrderModel>> GetCategoriaByIdAsync(int id)
         {
             throw new NotImplementedException();
         }
 
-        public async Task UpdateAsync(Domain.Entity.Order entity)
+        public Task<OperationResult<OrderModel>> UpdateCategoria(OrderModel model)
         {
             throw new NotImplementedException();
         }
