76ef96a12fa020f7e76dee95770a049a42c854a1
Atlast19 <ahleandro18@gmail.com>
2025-09-10 02:05:25 -0400

    Ordernes

---
+++ ShopApp.Application/Service/OrderService/OrderService.cs
@@ -0,0 +1,34 @@
+﻿using ShopApp.Application.Interface.Order;
+using ShopApp.Domain.Base;
+using ShopApp.Domain.Models.Order.OrderBaseModel;
+
+namespace ShopApp.Application.Service.OrderService
+{
+    public class OrderService : IOrderService
+    {
+        public Task<OperationResult<OrderModel>> CreateOrderAsync(OrderModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult<OrderModel>> DeleteOrderByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult<List<OrderModel>>> GetAllOrderAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult<OrderModel>> GetOrderByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult<OrderModel>> UpdateOrder(OrderModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
